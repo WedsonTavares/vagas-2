36526ef9de0f40641fca06410eeaaccbfbc6839e
Wedson Tavares <wedsonsobral@gmail.com>
2025-05-22 23:18:31 -0300

    fix logo icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,6 +16,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Controle de Vagas",
   description: "Sistema para gerenciar inscrições em vagas de estágio e júnior",
+  icons: "/icone.png",
 };
 
 export default function RootLayout({
@@ -24,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 text-gray-900 min-h-screen`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 text-gray-900 min-h-screen`}>
         <main className="max-w-3xl mx-auto p-4">
           <h1 className="text-3xl font-bold text-center mb-6">Controle de Vagas</h1>
           {children}
